08a1e7ff45056bb560fccc83fbdbe0f5dda168b5
thisd92 <80495413+thisd92@users.noreply.github.com>
2022-11-15 12:59:34 -0300

    responsive flexbox

--- js/vanilla.js
+++ js/vanilla.js
@@ -1,20 +1,36 @@
 window.onload = () => (verificaUrl())
 
-function verificaUrl(){
+function verificaUrl() {
     let doc = window.location.href.toString()
-    if(doc.includes('create')){
-        const li = document.getElementsByClassName('list')
-        li[1].className = "active"
-    }else if(doc.includes('read')){
-        const li = document.getElementsByClassName('list')
-        li[2].className = "active"
-    
-    }else if(doc.includes('update')){
-        const li = document.getElementsByClassName('list')
-        li[3].className = "active"
-    
-    }else if(doc.includes('delete')){
-        const li = document.getElementsByClassName('list')
-        li[4].className = "active"
+    if (doc.includes('create')) {
+        const create = document.getElementById('create')
+        create.className += " active"
+    } else if (doc.includes('read')) {
+        const read = document.getElementById('read')
+        read.className += " active"
+    } else if (doc.includes('update')) {
+        const update = document.getElementById('update')
+        update.className += " active"
+    } else if (doc.includes('delete')) {
+        const del = document.getElementById('del')
+        del.className += " active"
+    }
+}
+
+function toggleDrop() {
+    const navbar = document.getElementById('navbar')
+    const menuBtn1 = document.querySelector('.menu-btn1')
+    const menuBtn2 = document.querySelector('.menu-btn2')
+    const menuBtn3 = document.querySelector('.menu-btn3')
+    if(navbar.style.display !== "flex"){
+        navbar.style.display = 'flex'
+        menuBtn1.classList.toggle('active-btn')
+        menuBtn2.classList.toggle('active-btn')
+        menuBtn3.classList.toggle('active-btn')
+    }else{
+        navbar.style.display = 'none'
+        menuBtn1.classList.toggle('active-btn')
+        menuBtn2.classList.toggle('active-btn')
+        menuBtn3.classList.toggle('active-btn')
     }
 }
