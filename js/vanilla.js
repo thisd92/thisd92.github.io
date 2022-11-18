3dd4185161df57278f1a09b56be81ff7f0846973
Thiago Dutra <thiiagodutra@gmail.com>
2022-11-18 14:12:32 -0300

    responsive flexbox

--- js/vanilla.js
+++ js/vanilla.js
@@ -21,16 +21,26 @@ function toggleDrop() {
     const navbar = document.getElementById('navbar')
     const menuBtn1 = document.querySelector('.menu-btn1')
     const menuBtn2 = document.querySelector('.menu-btn2')
-    const menuBtn3 = document.querySelector('.menu-btn3')
     if(navbar.style.display !== "flex"){
         navbar.style.display = 'flex'
         menuBtn1.classList.toggle('active-btn')
         menuBtn2.classList.toggle('active-btn')
-        menuBtn3.classList.toggle('active-btn')
     }else{
         navbar.style.display = 'none'
         menuBtn1.classList.toggle('active-btn')
         menuBtn2.classList.toggle('active-btn')
-        menuBtn3.classList.toggle('active-btn')
+    }
+}
+
+document.body.onresize = () => {
+    const elNav = document.getElementById('navbar')
+    const menuBtn1 = document.querySelector('.menu-btn1')
+    const menuBtn2 = document.querySelector('.menu-btn2')
+    if(document.body.clientWidth > 540){
+        elNav.style.display = 'flex';
+    } else{
+        elNav.style.display = 'none';
+        menuBtn1.classList.remove('active-btn')
+        menuBtn2.classList.remove('active-btn')
     }
 }
